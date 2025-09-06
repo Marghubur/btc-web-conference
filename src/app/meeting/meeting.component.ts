488546790ed971d3e4e3bf6cc4c3450e4826798f
Marghubur <marghub12@gmail.com>
2025-09-06 18:07:34 +0530

    tootltip removed

--- src/app/meeting/meeting.component.ts
+++ src/app/meeting/meeting.component.ts
@@ -14,12 +14,11 @@ import { User } from '../preview/preview.component';
 import { LocalService } from '../services/local.service';
 import { ScreenRecorderService } from '../services/screen-recorder.service';
 import { hand_down, hand_raise } from '../services/constant';
-import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-meeting',
     standalone: true,
-    imports: [FormsModule, ReactiveFormsModule, AudioComponent, VideoComponent, CommonModule, NgbTooltipModule],
+    imports: [FormsModule, ReactiveFormsModule, AudioComponent, VideoComponent, CommonModule],
     templateUrl: './meeting.component.html',
     styleUrl: './meeting.component.css'
 })
