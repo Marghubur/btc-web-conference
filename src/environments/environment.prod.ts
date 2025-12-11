572de2fbb901f6de6a05755a6a8a8b9fcfda639e
BottomHalf <istiyaq.mi9@gmail.com>
2025-12-11 10:45:50 +0530

    added urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
   sfuBaseUrl: "www.confeet.com",
-  appServerBaseUrl: `https://www.confeet.com/api/`
+  appServerBaseUrl: `https://www.confeet.com/api/`,
+
+  socketBaseUrl: "ws://www.confeet.com/socket",
+  messageBaseUrl: "http://www.confeet.com/message/",
+  socketHandshakEndpoint: "ws",
 };
