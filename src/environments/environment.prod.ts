513c2c202a4a86d9fbaa3c32eb076f61d8e84c58
BottomHalf <istiyaq.mi9@gmail.com>
2025-12-11 13:32:39 +0530

    added service/api route

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   appServerBaseUrl: `https://www.confeet.com/api/`,
 
   socketBaseUrl: "wss://www.confeet.com/cf/meet",
-  messageBaseUrl: "https://www.confeet.com/cf/meet/meetings/",
+  messageBaseUrl: "https://www.confeet.com/meetings/api/",
   socketHandshakEndpoint: "ws",
 };
